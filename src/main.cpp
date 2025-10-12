ae4013eec2914fe5b2df796cdaa4ac081c54dabd
matty <mattreed1890@gmail.com>
2025-10-12 16:03:44 -0500

    add opeGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,3 @@
-#include "bulkin.h"
-#include "level.h"
 #include <glm/glm.hpp>
 
-int main() {
-  Bulkin app;
-  auto wallTexture = app.addTexture("textures/red_brick_diff_4k.jpg");
-  auto boxTexture = app.addTexture("textures/brown_floor_tiles_diff_4k.jpg");
-  auto ceilingTexture = app.addTexture("textures/plastered_wall_diff_4k.jpg");
-  BulkinLevel level("resources/level.tmx", wallTexture, boxTexture,
-                    ceilingTexture, 2);
-  level.renderLevel(app);
-  app.addModel("resources/counter_top/scene.gltf", {5.0f, 0.003f, 10.0f}, 90.0f,
-               {0.0f, 1.0f, 0.0f}, 0.006f);
-  app.run();
-}
+int main() {}
