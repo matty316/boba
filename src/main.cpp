8686c757fc1538c719b3ad9a69a9427cf2f85363
matty <mattreed1890@gmail.com>
2025-10-30 11:49:56 -0500

    models

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,22 @@
 #ifdef OPEGL
-#include <level.hpp>
 #include <opegl.hpp>
 #else
 #include "bulkin.h"
-#include "level.h"
-#include <glm/glm.hpp>
 #endif
 
 int main() {
 #ifdef OPEGL
   OpeGL app;
+#else
+  Bulkin app;
+#endif
+  app.addModel("resources/counter_top/scene.gltf", {5.0f, 0.003f, 10.0f}, 90.0f,
+               {0.0f, 1.0f, 0.0f}, 0.006f);
+
   auto wallTexture = app.addTexture("textures/red_brick_diff_4k.jpg");
   auto floorTexture = app.addTexture("textures/brown_floor_tiles_diff_4k.jpg");
   auto ceilingTexture = app.addTexture("textures/plastered_wall_diff_4k.jpg");
-  OpeLevel level("resources/level.tmx", wallTexture, floorTexture,
-                 ceilingTexture, 2);
-  level.loadLevel(app);
-
-#else
-  Bulkin app;
-  app.addModel("resources/counter_top/scene.gltf", {5.0f, 0.003f, 10.0f}, 90.0f,
-               {0.0f, 1.0f, 0.0f}, 0.006f);
-  auto wallTexture = app.addTexture("textures/red_brick_diff_4k.jpg");
-  auto boxTexture = app.addTexture("textures/brown_floor_tiles_diff_4k.jpg");
-  auto ceilingTexture = app.addTexture("textures/plastered_wall_diff_4k.jpg");
-  BulkinLevel level("resources/level.tmx", wallTexture, boxTexture,
-                    ceilingTexture, 2);
-  level.renderLevel(app);
-
-#endif
-
+  app.loadLevel("resources/level.tmx", wallTexture, floorTexture,
+                ceilingTexture, 2);
   app.run();
 }
