9062e5a3e6c47af62cac69b2e30f9294a31f4e9e
matty <mattreed1890@gmail.com>
2025-11-03 16:37:45 -0600

    add build

---
+++ build/opeGL/CMakeFiles/opeGL.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/matty/boba/opeGL/src/glad.c" "opeGL/CMakeFiles/opeGL.dir/src/glad.c.o" "gcc" "opeGL/CMakeFiles/opeGL.dir/src/glad.c.o.d"
+  "/home/matty/boba/opeGL/src/camera.cpp" "opeGL/CMakeFiles/opeGL.dir/src/camera.cpp.o" "gcc" "opeGL/CMakeFiles/opeGL.dir/src/camera.cpp.o.d"
+  "/home/matty/boba/opeGL/src/error.cpp" "opeGL/CMakeFiles/opeGL.dir/src/error.cpp.o" "gcc" "opeGL/CMakeFiles/opeGL.dir/src/error.cpp.o.d"
+  "/home/matty/boba/opeGL/src/level.cpp" "opeGL/CMakeFiles/opeGL.dir/src/level.cpp.o" "gcc" "opeGL/CMakeFiles/opeGL.dir/src/level.cpp.o.d"
+  "/home/matty/boba/opeGL/src/model.cpp" "opeGL/CMakeFiles/opeGL.dir/src/model.cpp.o" "gcc" "opeGL/CMakeFiles/opeGL.dir/src/model.cpp.o.d"
+  "/home/matty/boba/opeGL/src/opegl.cpp" "opeGL/CMakeFiles/opeGL.dir/src/opegl.cpp.o" "gcc" "opeGL/CMakeFiles/opeGL.dir/src/opegl.cpp.o.d"
+  "/home/matty/boba/opeGL/src/quad.cpp" "opeGL/CMakeFiles/opeGL.dir/src/quad.cpp.o" "gcc" "opeGL/CMakeFiles/opeGL.dir/src/quad.cpp.o.d"
+  "/home/matty/boba/opeGL/src/shader.cpp" "opeGL/CMakeFiles/opeGL.dir/src/shader.cpp.o" "gcc" "opeGL/CMakeFiles/opeGL.dir/src/shader.cpp.o.d"
+  "/home/matty/boba/opeGL/src/texture.cpp" "opeGL/CMakeFiles/opeGL.dir/src/texture.cpp.o" "gcc" "opeGL/CMakeFiles/opeGL.dir/src/texture.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
