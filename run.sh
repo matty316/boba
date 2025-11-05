e3ea47c156a0eb32a6501134c11106aa97218b19
matty <mattreed1890@gmail.com>
2025-11-05 12:15:32 -0600

    update run script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 $1
-make
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 $1 -GNinja
+ninja
 cp -r ../resources ../textures .
 ./boba
