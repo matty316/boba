29d823099dbd4b324c1e2d08db60e632fd9f6382
matty <mattreed1890@gmail.com>
2025-11-05 11:01:57 -0600

    add cube

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,15 @@
 int main() {
 #ifdef OPEGL
   OpeGL app;
+
 #else
   Bulkin app;
 #endif
-  app.addModel("resources/counter_top/scene.gltf", {5.0f, 0.003f, 10.0f}, 90.0f,
-               {0.0f, 1.0f, 0.0f}, 0.006f);
 
   auto wallTexture = app.addTexture("textures/red_brick_diff_4k.jpg");
   auto floorTexture = app.addTexture("textures/brown_floor_tiles_diff_4k.jpg");
   auto ceilingTexture = app.addTexture("textures/plastered_wall_diff_4k.jpg");
+  app.addCube(3, 3, wallTexture);
   app.loadLevel("resources/level.tmx", wallTexture, floorTexture,
                 ceilingTexture, 2);
   app.run();
